1a644b77abdd83feeb7da03015248e63faa9ffda
Luan-Thanhh <luanthanh0322@gmail.com>
2023-03-09 20:26:54 +0700

    feat: setup project

--- config/connect_db.js
+++ config/connect_db.js
@@ -4,9 +4,7 @@ mongoose.set('strictQuery', true);
 
 async function connect() {
   try {
-    await mongoose.connect(
-      'mongodb+srv://profile:profileSmile@cluster0.d0dy7bi.mongodb.net/db_cv'
-    );
+    await mongoose.connect('mongodb+srv://cvSmile:cvSmile123@mycv.ynodbx7.mongodb.net/cv_db');
     console.log('Database connected!!');
   } catch (error) {
     console.log('Database not connected!!');
